e24d2be32960fbca283b5fd64cc00cfa36f512e4
aberejnec <adrian.berejnec@irian.ro>
2023-10-30 18:30:59 +0200

    fix cyclic dependencies

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import Screen1 from "./Screen1";
-import Screen2 from "./Screen2";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, TextInput } from "react-native";
+import "firebase/database";
+import { getDocs } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCbEcPCbwWFRHgMBjHpup9Z-UDRa84JuCc",
@@ -21,6 +24,80 @@ const db = getFirestore();
 export { db };
 
 const Stack = createStackNavigator();
+
+const Screen1 = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("");
+  const handleGreet = () => {
+    if (name) {
+      setGreeting(`Hello, ${name}`);
+    } else {
+      setGreeting("");
+    }
+  };
+
+  useEffect(() => {
+    const textsCollection = collection(db, "texts");
+
+    getDocs(textsCollection).then((snapshot) => {
+      let collection = [];
+      snapshot.docs.forEach((doc) => {
+        collection.push({ ...doc.data(), id: doc.id });
+      });
+      setName(collection[1]?.title);
+    });
+  });
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <Button
+        title="Go to Screen 2"
+        onPress={() => navigation.navigate("Screen2")}
+      />
+      <TextInput
+        placeholder="The text from Firebase will be shown here"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <Button color="red" title="Greet me" onPress={handleGreet} />
+      <Text>{greeting}</Text>
+    </View>
+  );
+};
+
+const Screen2 = () => {
+  const [text, setText] = useState("");
+
+  const saveTextToDatabase = async () => {
+    await addDoc(collection(db, "texts"), {
+      title: text,
+    });
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <TextInput
+        placeholder="Enter text"
+        onChangeText={(newText) => setText(newText)}
+      />
+      <Button title="Save Text to Firebase" onPress={saveTextToDatabase} />
+    </View>
+  );
+};
 
 const App = () => {
   return (
--- Screen1.js
+++ Screen1.js
@@ -4,51 +4,51 @@ import "firebase/database";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./App";
 
-const Screen1 = ({ navigation }) => {
-  const [name, setName] = useState("");
-  const [greeting, setGreeting] = useState("");
-  const handleGreet = () => {
-    if (name) {
-      setGreeting(`Hello, ${name}`);
-    } else {
-      setGreeting("");
-    }
-  };
+// const Screen1 = ({ navigation }) => {
+//   const [name, setName] = useState("");
+//   const [greeting, setGreeting] = useState("");
+//   const handleGreet = () => {
+//     if (name) {
+//       setGreeting(`Hello, ${name}`);
+//     } else {
+//       setGreeting("");
+//     }
+//   };
 
-  useEffect(() => {
-    const textsCollection = collection(db, "texts");
+//   useEffect(() => {
+//     const textsCollection = collection(db, "texts");
 
-    getDocs(textsCollection).then((snapshot) => {
-      let collection = [];
-      snapshot.docs.forEach((doc) => {
-        collection.push({ ...doc.data(), id: doc.id });
-      });
-      setName(collection[0]?.title);
-    });
-  });
+//     getDocs(textsCollection).then((snapshot) => {
+//       let collection = [];
+//       snapshot.docs.forEach((doc) => {
+//         collection.push({ ...doc.data(), id: doc.id });
+//       });
+//       setName(collection[1]?.title);
+//     });
+//   });
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 10,
-      }}
-    >
-      <Button
-        title="Go to Screen 2"
-        onPress={() => navigation.navigate("Screen2")}
-      />
-      <TextInput
-        placeholder="Enter your name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <Button color="red" title="Greet me" onPress={handleGreet} />
-      <Text>{greeting}</Text>
-    </View>
-  );
-};
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//         gap: 10,
+//       }}
+//     >
+//       <Button
+//         title="Go to Screen 2"
+//         onPress={() => navigation.navigate("Screen2")}
+//       />
+//       <TextInput
+//         placeholder="Enter your name"
+//         value={name}
+//         onChangeText={(text) => setName(text)}
+//       />
+//       <Button color="red" title="Greet me" onPress={handleGreet} />
+//       <Text>{greeting}</Text>
+//     </View>
+//   );
+// };
 
-export default Screen1;
+// export default Screen1;
